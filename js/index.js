58a249dfd118045101e563435e5505deffc2686e
VCLdesigns <Bart.Victor.Loots@gmail.com>
2022-11-23 20:02:43 +0100

    fixied nav

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 var menuButton = document.getElementById("menu_button");
 var myTopNav = document.getElementById("nav_links");
-var crossBars = document.querySelector("i");
+
 menuButton.addEventListener("click", openCloseMenu);
 
 function openCloseMenu() {
-   menuButton.classList.toggle("menu_button_color");
+
+    menuButton.classList.toggle("menu_button_color");
     myTopNav.classList.toggle("nav_links_hidden");
 }
+
 
 
